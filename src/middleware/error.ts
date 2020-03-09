f276f8efc64e8fa7ed20761b97b0e9c95b5b2489
ce2kettu <teemu.ket@gmail.com>
2020-03-09 20:50:57 +0200

    Use http status codes in error handler

--- src/middleware/error.ts
+++ src/middleware/error.ts
@@ -1,5 +1,6 @@
 import { NextFunction, Request, Response } from 'express';
 import { API, env, HttpException } from '../util';
+import { INTERNAL_SERVER_ERROR, OK } from 'http-status-codes';
 
 export function routeNotFound(req: Request, res: Response, next: NextFunction): void {
     API.error(res, 'Not Found', 404);
@@ -8,10 +9,12 @@ export function routeNotFound(req: Request, res: Response, next: NextFunction): 
 export function errorMiddleware(err: HttpException, req: Request, res: Response, next: NextFunction): void {
     if (!res.headersSent) {
         if (env.NODE_ENV === 'development') {
-            res.json({ success: false, message: err.message, stack: err.stack });
+            res.status(OK).json({ success: false, message: err.message, stack: err.stack });
         } else {
             const isAllowedException = err.name = 'HttpException';
-            res.json({ success: false, message: isAllowedException ? err.message : 'Something went wrong' });
+            res.status(INTERNAL_SERVER_ERROR).json({
+                success: false, message: isAllowedException ? err.message : 'Something went wrong',
+            });
         }
     }
 }
