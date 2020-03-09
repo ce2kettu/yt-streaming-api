63800ec54da56b007e068250e5b108038e0079be
ce2kettu <teemu.ket@gmail.com>
2020-03-09 20:13:02 +0200

    Add http status code parameter to api

--- src/middleware/index.ts
+++ src/middleware/index.ts
@@ -1,3 +1,4 @@
 export * from './apiKey';
 export * from './error';
 export * from './youtubeValidation';
+export * from './timeout';
--- src/util/api.ts
+++ src/util/api.ts
@@ -1,16 +1,19 @@
 import { Response } from 'express';
+import HttpStatus from 'http-status-codes';
 
 export class API {
-    public static response(res: Response, message?: string, data?: object) {
+    public static response(res: Response, message?: string, data?: object, status?: number) {
         message = message || '';
         data = data || null;
+        status = status || HttpStatus.OK;
 
-        res.json({ success: true, message, data });
+        res.status(status).json({ success: true, message, data });
     }
 
-    public static error(res: Response, message?: string) {
+    public static error(res: Response, message?: string, status?: number) {
         message = message || 'An error occurred.';
+        status = status || HttpStatus.INTERNAL_SERVER_ERROR;
 
-        res.json({ success: false, message });
+        res.status(status).json({ success: false, message });
     }
 }
